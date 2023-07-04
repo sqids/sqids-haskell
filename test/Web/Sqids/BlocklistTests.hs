a549ad8d0e9c1c1443577eff19ff116dfd6e1ee1
Heikki Johannes Hildén <hildenjohannes@gmail.com>
2023-07-04 17:16:43 +0300

    wip

--- test/Web/Sqids/BlocklistTests.hs
+++ test/Web/Sqids/BlocklistTests.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Web.Sqids.BlocklistTests where
 
+import Control.Monad ((>=>))
 import Data.Text (Text)
 import Test.Hspec
 import Web.Sqids.Internal
@@ -33,9 +34,10 @@ testBlocklist = do
       withNonEmptyBlocklist (encode [ 100000 ]) `shouldBe` Right "7T1X8k"
       withNonEmptyBlocklist (decode "7T1X8k") `shouldBe` Right [ 100000 ]
 
+    -- TODO: More descriptive message?
     it "blocklist" $ do
-      let blocklist = 
-            [ "8QRLaD"     -- Normal result of first encoding, let's block that word on purpose
+      let blocklist =
+            [ "8QRLaD"     -- Normal result of first encoding -- Let's block that word on purpose
             , "7T1cd0dL"   -- Result of second encoding
             , "UeIe"       -- Result of third encoding is `RA8UeIe7` - Let's block a substring
             , "imhw"       -- Result of 4th encoding is `WM3Limhw` - Let's block the postfix
@@ -44,43 +46,20 @@ testBlocklist = do
       withCustomBlocklist blocklist (encode [1, 2, 3]) `shouldBe` Right "TM0x1Mxz"
       withCustomBlocklist blocklist (decode "TM0x1Mxz") `shouldBe` Right [1, 2, 3]
 
---    it "decoding blocklist words should still work" $ do
---
---    it "match against a short blocklist word" $ do
+    it "decoding blocklist words should still work" $ do
+      let blocklist =
+            [ "8QRLaD"
+            , "7T1cd0dL"
+            , "RA8UeIe7"
+            , "WM3Limhw"
+            , "LfUQh4HN"
+            ]
+      withCustomBlocklist blocklist (decode "8QRLaD") `shouldBe` Right [1, 2, 3]
+      withCustomBlocklist blocklist (decode "7T1cd0dL") `shouldBe` Right [1, 2, 3]
+      withCustomBlocklist blocklist (decode "RA8UeIe7") `shouldBe` Right [1, 2, 3]
+      withCustomBlocklist blocklist (decode "WM3Limhw") `shouldBe` Right [1, 2, 3]
+      withCustomBlocklist blocklist (decode "LfUQh4HN") `shouldBe` Right [1, 2, 3]
 
---
---test('blocklist', () => {
---	const sqids = new Sqids({
---		blocklist: new Set([
---			'8QRLaD', // normal result of 1st encoding, let's block that word on purpose
---			'7T1cd0dL', // result of 2nd encoding
---			'UeIe', // result of 3rd encoding is `RA8UeIe7`, let's block a substring
---			'imhw', // result of 4th encoding is `WM3Limhw`, let's block the postfix
---			'LfUQ' // result of 4th encoding is `LfUQh4HN`, let's block the prefix
---		])
---	});
---
---	expect.soft(sqids.encode([1, 2, 3])).toBe('TM0x1Mxz');
---	expect.soft(sqids.decode('TM0x1Mxz')).toEqual([1, 2, 3]);
---});
---
---test('decoding blocklist words should still work', () => {
---	const sqids = new Sqids({
---		blocklist: new Set(['8QRLaD', '7T1cd0dL', 'RA8UeIe7', 'WM3Limhw', 'LfUQh4HN'])
---	});
---
---	expect.soft(sqids.decode('8QRLaD')).toEqual([1, 2, 3]);
---	expect.soft(sqids.decode('7T1cd0dL')).toEqual([1, 2, 3]);
---	expect.soft(sqids.decode('RA8UeIe7')).toEqual([1, 2, 3]);
---	expect.soft(sqids.decode('WM3Limhw')).toEqual([1, 2, 3]);
---	expect.soft(sqids.decode('LfUQh4HN')).toEqual([1, 2, 3]);
---});
---
---test('match against a short blocklist word', () => {
---	const sqids = new Sqids({
---		blocklist: new Set(['pPQ'])
---	});
---
---	expect.soft(sqids.decode(sqids.encode([1000]))).toEqual([1000]);
---});
-
+    it "match against a short blocklist word" $ do
+      let blocklist = [ "pPQ" ]
+      withCustomBlocklist blocklist ((encode >=> decode) [1000]) `shouldBe` Right [1000]
